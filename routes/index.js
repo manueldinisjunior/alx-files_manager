c32c9653c3d3585bbe917c3cb90aab0fc450338c
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-12-03 19:21:55 +0200

    Index

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,22 @@
-import express from 'express';
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+import UsersController from '../controllers/UsersController';
 
-import { getStatus, getStats } from '../controllers/AppController';
-import { postNew, getMe } from '../controllers/UsersController';
-import { getConnect, getDisconnect } from '../controllers/AuthController';
-import {
-  postUpload, getShow, getIndex, putPublish, putUnPublish, getFile,
-} from '../controllers/FilesController';
+const router = Router();
 
-const router = express.Router();
-
-router.get('/status', getStatus);
-router.get('/stats', getStats);
-router.post('/users', postNew);
-router.get('/users/me', getMe);
-router.get('/connect', getConnect);
-router.get('/disconnect', getDisconnect);
-router.post('/files', postUpload);
-router.get('/files', getIndex);
-router.get('/files/:id', getShow);
-router.put('/files/:id/publish', putPublish);
-router.put('/files/:id/unpublish', putUnPublish);
-router.get('/files/:id/data', getFile);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', AuthController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files', FilesController.getIndex);
+router.get('/files/:id', FilesController.getShow);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
